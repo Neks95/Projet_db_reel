c11c1455ed967e5bc1e114cd9dd35cb0dc0a402a
Neks95 <rakotomangaa.nekena@gmail.com>
2025-06-25 10:54:04 +0300

    commit pour v2 1)

--- pages/emp.php
+++ pages/emp.php
@@ -20,21 +20,21 @@ $emp = getEmp($id);
         <header>
             <h1 class="text-center mb-4 text-dark"> Departements et les employes</h1>
         </header>
-
         <main>
             <table class="mt-5 table table-hover table-dark ">
                 <thead class="table-light text-dark">
                     <tr>
+                        <th scope="col">Numero</th>
                         <th scope="col">Nom</th>
                         <th scope="col">Prenom</th>
-                        <th scope="col">Sexe</th>f
+
                     </tr>
                 </thead>
                 <?php foreach ($emp as $empp) { ?>
                     <tr>
-                        <td><?php echo $empp['first_name'] ?></td>
-                        <td><?php echo $empp['last_name'] ?></td>
-                        <td><?php echo $empp['gender'] ?></td>
+                        <td><?php echo $empp['emp_no']?></td>
+                        <td><a href="fiche.php"><?php echo $empp['first_name']?></a></td>
+                        <td><?php echo $empp['last_name'] ?></td>                  
                         </td>
                     </tr>
                 <?php } ?>
